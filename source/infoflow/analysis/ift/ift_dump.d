30a02cc37721b75071ca94d6e50e75d69553a485
Ashley Wang <ashleyswang@ucsb.edu>
2022-10-19 18:33:45 -0700

    comment out log info not related to prop

--- source/infoflow/analysis/ift/ift_dump.d
+++ source/infoflow/analysis/ift/ift_dump.d
@@ -27,7 +27,7 @@ template IFTAnalysisDump(TRegWord, TMemWord, TRegSet) {
 
         void dump_clobber() {
             // 1. dump clobber commit
-            writefln(" clobber (%s commits):", ift.trace.commits.length);
+            // writefln(" clobber (%s commits):", ift.trace.commits.length);
 
             auto clobbered_reg_ids = ift.clobber.get_effect_reg_ids().array;
             auto clobbered_reg_values = ift.clobber.get_effect_reg_values().array;
@@ -38,40 +38,40 @@ template IFTAnalysisDump(TRegWord, TMemWord, TRegSet) {
 
             if (ift.included_data & IFTAnalyzer.IFTDataType.Memory) {
                 // memory
-                writefln("  memory:");
+                // writefln("  memory:");
                 for (auto i = 0; i < clobbered_mem_addrs.length; i++) {
                     auto mem_addr = clobbered_mem_addrs[i];
                     auto mem_value = clobbered_mem_values[i];
-                    writefln("   mem[$%08x] <- $%02x", mem_addr, mem_value);
+                    // writefln("   mem[$%08x] <- $%02x", mem_addr, mem_value);
                 }
             }
 
             if (ift.included_data & IFTAnalyzer.IFTDataType.Registers) {
                 // registers
-                writefln("  regs:");
+                // writefln("  regs:");
                 for (auto i = 0; i < clobbered_reg_ids.length; i++) {
                     auto reg_id = clobbered_reg_ids[i].to!TRegSet;
                     auto reg_value = clobbered_reg_values[i];
-                    writefln("   reg %s <- $%08x", reg_id, reg_value);
+                    // writefln("   reg %s <- $%08x", reg_id, reg_value);
                 }
             }
 
             if (ift.included_data & IFTAnalyzer.IFTDataType.CSR) {
                 // csr
-                writefln("  csr:");
+                // writefln("  csr:");
                 for (auto i = 0; i < clobbered_csr_ids.length; i++) {
                     auto csr_id = clobbered_csr_ids[i];
                     auto csr_value = clobbered_csr_values[i];
-                    writefln("   csr#%x <- $%08x", csr_id, csr_value);
+                    // writefln("   csr#%x <- $%08x", csr_id, csr_value);
                 }
             }
 
             auto total_clobber_nodes =
                 clobbered_reg_ids.length + clobbered_mem_addrs.length + clobbered_csr_ids.length;
-            writefln("  clobbered reg nodes: %s", clobbered_reg_ids.length);
-            writefln("  clobbered mem nodes: %s", clobbered_mem_addrs.length);
+            // writefln("  clobbered reg nodes: %s", clobbered_reg_ids.length);
+            // writefln("  clobbered mem nodes: %s", clobbered_mem_addrs.length);
             // writefln("  clobbered csr nodes: %s", clobbered_csr_ids.length);
-            writefln("  total clobbered nodes: %s", total_clobber_nodes);
+            // writefln("  total clobbered nodes: %s", total_clobber_nodes);
         }
 
         void dump_commits() {
@@ -86,7 +86,7 @@ template IFTAnalysisDump(TRegWord, TMemWord, TRegSet) {
         void dump_analysis() {
             import std.array : appender;
 
-            writefln("\nanalysis:");
+            // writefln("\nanalysis:");
 
             // dump backtraces
             // writefln(" backtraces:");
@@ -155,12 +155,12 @@ template IFTAnalysisDump(TRegWord, TMemWord, TRegSet) {
                 }
             }
 
-            writefln(" theoritical minimization:");
+            // writefln(" theoritical minimization:");
             auto num_minimal_commits = minimal_commit_set.length;
-            writefln("  minimal commits: %s", num_minimal_commits);
-            writefln("  total commits: %s", ift.trace.commits.length);
-            writefln("  theoretical minimum untouched commits: %.2f%%",
-                (100.0 * num_minimal_commits) / ift.trace.commits.length);
+            // writefln("  minimal commits: %s", num_minimal_commits);
+            // writefln("  total commits: %s", ift.trace.commits.length);
+            // writefln("  theoretical minimum untouched commits: %.2f%%",
+                // (100.0 * num_minimal_commits) / ift.trace.commits.length);
         }
 
         void dump_graph() {
@@ -180,9 +180,9 @@ template IFTAnalysisDump(TRegWord, TMemWord, TRegSet) {
             // mixin(LOG_INFO!(`"  stats"`));
             // mixin(LOG_INFO!(`"   verts: %s", ift.ift_graph.num_verts`));
             // mixin(LOG_INFO!(`"   edges: %s", ift.ift_graph.num_edges`));
-            writefln("  stats:");
-            writefln("   verts: %s", ift.ift_graph.num_verts);
-            writefln("   edges: %s", ift.ift_graph.num_edges);
+            // writefln("  stats:");
+            // writefln("   verts: %s", ift.ift_graph.num_verts);
+            // writefln("   edges: %s", ift.ift_graph.num_edges);
 
             // // dump all ift subtrees
             // writefln(" dependency subtrees:");
@@ -215,7 +215,7 @@ template IFTAnalysisDump(TRegWord, TMemWord, TRegSet) {
             // g.save("simple.dot");
 
             auto g = new Directed();
-            writefln(" exporting ift graph as graphviz");
+            // writefln(" exporting ift graph as graphviz");
 
             with (g) {
                 foreach (ift_vert; ift.ift_graph.nodes) {
@@ -242,7 +242,7 @@ template IFTAnalysisDump(TRegWord, TMemWord, TRegSet) {
             }
 
             g.save(output);
-            writefln("  wrote graphviz data to %s", output);
+            // writefln("  wrote graphviz data to %s", output);
         }
 
         void dump_summary() {
@@ -251,33 +251,33 @@ template IFTAnalysisDump(TRegWord, TMemWord, TRegSet) {
             auto clobered_csr_ids = ift.clobber.get_effect_csr_ids().array;
 
             // summary
-            writefln(" summary:");
-            writefln("  num commits:            %8d", ift.trace.commits.length);
-            if (ift.included_data & IFTAnalyzer.IFTDataType.Registers) {
-                writefln("  registers traced:       %8d", clobbered_reg_ids.length);
-            }
-            if (ift.included_data & IFTAnalyzer.IFTDataType.Memory) {
-                writefln("  memory traced:          %8d", clobbered_mem_addrs.length);
-            }
-            if (ift.included_data & IFTAnalyzer.IFTDataType.CSR) {
-                writefln("  csr traced:             %8d", clobered_csr_ids.length);
-            }
+            // writefln(" summary:");
+            // writefln("  num commits:            %8d", ift.trace.commits.length);
+            // if (ift.included_data & IFTAnalyzer.IFTDataType.Registers) {
+            //     writefln("  registers traced:       %8d", clobbered_reg_ids.length);
+            // }
+            // if (ift.included_data & IFTAnalyzer.IFTDataType.Memory) {
+            //     writefln("  memory traced:          %8d", clobbered_mem_addrs.length);
+            // }
+            // if (ift.included_data & IFTAnalyzer.IFTDataType.CSR) {
+            //     writefln("  csr traced:             %8d", clobered_csr_ids.length);
+            // }
             version (analysis_log) {
-                writefln("  found sources:          %8d", ift.log_found_sources);
-                writefln("  walked info:            %8d", ift.log_visited_info_nodes);
-                writefln("  walked commits:         %8d", ift.log_commits_walked);
-                writefln("  walked graph nodes:     %8d", ift.log_graph_nodes_walked);
-                writefln("  graph cache hits:       %8d", ift.log_graph_nodes_cache_hits);
-                writefln("  graph cache misses:     %8d", ift.log_graph_nodes_cache_misses);
-                writefln("  node walk duplicates:   %8d", ift.log_global_node_walk_duplicates);
-                writefln("  cache build time:       %7ss", (
-                    cast(double) ift.log_cache_build_time / 1_000_000));
-                writefln("  propagation walked:     %8d", ift.log_propagation_nodes_walked);
+                // writefln("  found sources:          %8d", ift.log_found_sources);
+                // writefln("  walked info:            %8d", ift.log_visited_info_nodes);
+                // writefln("  walked commits:         %8d", ift.log_commits_walked);
+                // writefln("  walked graph nodes:     %8d", ift.log_graph_nodes_walked);
+                // writefln("  graph cache hits:       %8d", ift.log_graph_nodes_cache_hits);
+                // writefln("  graph cache misses:     %8d", ift.log_graph_nodes_cache_misses);
+                // writefln("  node walk duplicates:   %8d", ift.log_global_node_walk_duplicates);
+                // writefln("  cache build time:       %7ss", (
+                //     cast(double) ift.log_cache_build_time / 1_000_000));
+                // writefln("  propagation walked:     %8d", ift.log_propagation_nodes_walked);
                 writefln("  propagation time:       %7sms", (
                     cast(double) ift.log_propagation_time / 1_000));
             }
-            writefln("  analysis time:          %7ss", (
-                    cast(double) ift.log_analysis_time / 1_000_000));
+            // writefln("  analysis time:          %7ss", (
+            //         cast(double) ift.log_analysis_time / 1_000_000));
         }
     }
 }
